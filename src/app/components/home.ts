3709e6ea7e7a9a5c1a91e95a4e03d53312f6e320
Pedro José Peña <pjpena@stratio.com>
2017-02-27 09:46:20 +0100

    Remove Example

--- src/app/components/home.ts
+++ src/app/components/home.ts
@@ -1,16 +1,9 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
    selector: 'page-home',
    templateUrl: 'home.html'
 })
-export class HomeComponent implements AfterViewInit {
+export class HomeComponent {
    constructor() { }
-
-   ngAfterViewInit(): void {
-      setTimeout(function() {
-          throw new Error('custom error');
-
-      }, 300);
-   }
 }
